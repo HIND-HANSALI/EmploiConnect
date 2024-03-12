8dd17277b4236c1f878f2a2ca57282baa07ec846
HIND-HANSALI <hindhsl111@gmail.com>
2024-03-12 09:29:50 +0100

    add packages and components

--- FrontEnd-EmploiConnect/src/app/app.module.ts
+++ FrontEnd-EmploiConnect/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { CreateOfferComponent } from './components/offer/create-offer/create-offer.component';
+import { ListOffersComponent } from './components/offer/list-offers/list-offers.component';
+import { ListApplicationsComponent } from './components/application/list-applications/list-applications.component';
+import { RegisterAppliactionComponent } from './components/application/register-appliaction/register-appliaction.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CreateOfferComponent,
+    ListOffersComponent,
+    ListApplicationsComponent,
+    RegisterAppliactionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
