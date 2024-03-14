121b68c162f103be821d5310162c096711b5d7be
HIND-HANSALI <hindhsl111@gmail.com>
2024-03-14 11:14:01 +0100

    add routings

--- FrontEnd-EmploiConnect/src/app/app-routing.module.ts
+++ FrontEnd-EmploiConnect/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ListOffersComponent } from './components/offer/list-offers/list-offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'offers',component: ListOffersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
