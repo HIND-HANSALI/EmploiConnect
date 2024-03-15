9a7411e3a1812cad7ade4952d08679bb61a0e50c
HIND-HANSALI <hindhsl111@gmail.com>
2024-03-15 14:08:14 +0000

    implement security in login

--- FrontEnd-EmploiConnect/src/app/components/auth/login/login.component.ts
+++ FrontEnd-EmploiConnect/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginDto } from 'src/app/dtos/requests/LoginDto';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string= '';
+  password: string= '';
+
+  constructor(private authService: AuthService, private route: Router) { }
+
+  onSubmit() {
+    const loginDto: LoginDto = {
+      email : this.email,
+      password : this.password,
+    }
+    this.authService.login(loginDto).subscribe(
+      (response) => {
+        console.log('Login successful!', response);
+
+        localStorage.setItem('role', response.role.name);
+
+        this.route.navigate(["home"])
+      },
+      (error) => {
+        
+        console.error('Login failed!', error);
+      }
+    );
+  }
+
 
 }
--- FrontEnd-EmploiConnect/src/app/services/auth/auth.service.ts
+++ FrontEnd-EmploiConnect/src/app/services/auth/auth.service.ts
@@ -1,9 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { LoginDto } from 'src/app/dtos/requests/LoginDto';
+import { RegisterDto } from 'src/app/dtos/requests/RegisterDto';
+import { AuthenticationResponseDTO } from 'src/app/dtos/responses/AuthenticationResponseDTO';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllUsers(): Observable<AuthenticationResponseDTO[]> {
+    return this.http.get<AuthenticationResponseDTO[]>(`${environment.apiUrl}/auth`);
+  }
+
+  login(user: LoginDto): Observable<AuthenticationResponseDTO> {
+    return this.http.post<AuthenticationResponseDTO>(`${environment.apiUrl}/auth/authenticate`, user)
+      .pipe(
+        map(response => {
+          
+          this.setToken(response.token);
+
+          return response;
+        })
+      );
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  removeToken(): void {
+    localStorage.removeItem('token');
+  }
+
+
+  register(user: RegisterDto): Observable<string>{
+    this.removeToken()
+    return this.http.post<{ token : string }>(`${environment.apiUrl}/auth/register`, user)
+      .pipe(
+         map(response => {
+           this.setToken(response.token)
+           return response.token
+         }),
+      );
+  }
 }
