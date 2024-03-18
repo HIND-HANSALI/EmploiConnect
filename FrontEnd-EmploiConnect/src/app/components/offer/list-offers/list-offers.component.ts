8a8d714c30bbd9fe2ca57b68e15bc90f7fb4b4eb
HIND-HANSALI <hindhsl111@gmail.com>
2024-03-18 09:38:45 +0000

    list offers cote client

--- FrontEnd-EmploiConnect/src/app/components/offer/list-offers/list-offers.component.ts
+++ FrontEnd-EmploiConnect/src/app/components/offer/list-offers/list-offers.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfferResponseDTO } from 'src/app/dtos/responses/OfferResponseDTO';
+import { OfferService } from 'src/app/services/offer/offer.service';
 
 @Component({
   selector: 'app-list-offers',
   templateUrl: './list-offers.component.html',
   styleUrls: ['./list-offers.component.css']
 })
-export class ListOffersComponent {
+export class ListOffersComponent implements OnInit {
+  offers: OfferResponseDTO[]= [];
 
+  constructor(private offerService: OfferService) { }
+
+  ngOnInit(): void {
+    this.loadOffers();
+  }
+
+  loadOffers(): void {
+    this.offerService.getAllOffers().subscribe((offers: OfferResponseDTO[]) => {
+        this.offers = offers;
+        console.log(offers);
+      },
+      (error) => {
+        console.error('Error fetching offers:', error);
+      }
+    );
+  }
 }
