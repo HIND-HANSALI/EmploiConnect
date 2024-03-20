365d8cf4890baca8aa052ea1a92411dc34634356
HIND-HANSALI <hindhsl111@gmail.com>
2024-03-20 09:30:50 +0000

    add create offer and update it

--- FrontEnd-EmploiConnect/src/app/components/offer/create-offer/create-offer.component.ts
+++ FrontEnd-EmploiConnect/src/app/components/offer/create-offer/create-offer.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OfferRequestDTO } from 'src/app/dtos/requests/OfferRequestDTO';
+import { NavbarService } from 'src/app/services/navbar/navbar.service';
+import { OfferService } from 'src/app/services/offer/offer.service';
 
 @Component({
   selector: 'app-create-offer',
   templateUrl: './create-offer.component.html',
   styleUrls: ['./create-offer.component.css']
 })
-export class CreateOfferComponent {
+export class CreateOfferComponent  implements OnInit{
+  // @Output() offerAdded: EventEmitter<OfferRequestDTO> = new EventEmitter<OfferRequestDTO>();
+  @Input() offerToUpdate: OfferRequestDTO | null = null;
+  offerForm: FormGroup;
+
+  isEditMode: boolean = false;
+
+  constructor(private navbarService: NavbarService,private fb: FormBuilder, private offerService: OfferService,private router: Router,private route: ActivatedRoute) {
+    this.offerForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      contrat: ['', Validators.required],
+    });
+  }
+ 
+  ngOnInit(): void {
+    this.navbarService.setShowNavbar(false);
+    const offerId = this.route.snapshot.paramMap.get('id');
+    if (offerId) {
+      this.isEditMode = true;
+      this.offerService.getOfferById(+offerId).subscribe(offer => {
+        this.offerToUpdate = offer;
+        console.log(this.offerToUpdate);
+        this.offerForm.patchValue({
+          title: offer.title,
+          description: offer.description,
+          contrat: offer.contrat
+        });
+      });
+    }else {
+      // Creating new offer
+      this.isEditMode = false;
+      this.offerForm.reset();
+    }
+  
+
+  }
+  get formTitle(): string {
+    return this.isEditMode ? 'Update Offer' : 'Create Offer';
+  }
+
+  get submitButtonText(): string {
+    return this.isEditMode ? 'Update Offer' : 'Save Offer';
+  }
+
+
+  onSubmit() {
+    const offerData: OfferRequestDTO = this.offerForm.value;
+    if (this.isEditMode) {
+      this.updateOffer(offerData);
+    } else {
+      this.saveOffer(offerData);
+    }
+  }
+  
+  private updateOffer(offerData: OfferRequestDTO) {
+    const offerId = this.route.snapshot.paramMap.get('id');
+    if (offerId && this.offerToUpdate) {
+      this.offerService.updateOffer(+offerId, offerData).subscribe(updatedOffer => {
+        console.log('Offer updated successfully:', updatedOffer);
+        this.router.navigate(['/dashboard/offers']);
+      });
+    }
+  }
+  
+  private saveOffer(offerData: OfferRequestDTO) {
+    this.offerService.saveOffer(offerData).subscribe(newOffer => {
+      console.log('Offer created successfully:', newOffer);
+      this.offerForm.reset();
+      this.router.navigate(['/dashboard/offers']);
+    });
+  }
+ 
+
+  // onSubmit() {
+  //   if (this.offerForm.valid) {
+  //     const offerToAdd: OfferRequestDTO = {
+  //       title: this.offerForm.value.title,
+  //       description: this.offerForm.value.description,
+  //       contrat: this.offerForm.value.contrat,
+  //       createdAt: new Date()
+  //     };
+     
 
 }
--- FrontEnd-EmploiConnect/src/app/services/offer/offer.service.ts
+++ FrontEnd-EmploiConnect/src/app/services/offer/offer.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OfferRequestDTO } from 'src/app/dtos/requests/OfferRequestDTO';
+import { OfferResponseDTO } from 'src/app/dtos/responses/OfferResponseDTO';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllOffers(): Observable<OfferResponseDTO[]> {
+
+    return this.http.get<OfferResponseDTO[]>(`${environment.apiUrl}/offers`);
+  }
+  
+  saveOffer(offer: OfferRequestDTO): Observable<OfferRequestDTO> {
+    return this.http.post<OfferRequestDTO>(`${environment.apiUrl}/offers`, offer);
+  }
+
+  updateOffer(offerId: number, offer: OfferRequestDTO): Observable<OfferRequestDTO> {
+    return this.http.put<OfferRequestDTO>(`${environment.apiUrl}/offers/${offerId}`, offer);
+  }
+  getOfferById(offerId: number): Observable<OfferResponseDTO> {
+    return this.http.get<OfferResponseDTO>(`${environment.apiUrl}/offers/${offerId}`);
+  }
+ 
+
 }
