fb1959b7f6537df7d70337f84b00b503826bf730
HIND-HANSALI <hindhsl111@gmail.com>
2024-04-01 11:10:48 +0000

    display other infos in offer list

--- FrontEnd-EmploiConnect/src/app/dtos/responses/OfferResponseDTO.ts
+++ FrontEnd-EmploiConnect/src/app/dtos/responses/OfferResponseDTO.ts
@@ -1,7 +1,10 @@
+import { AuthenticationResponseDTO } from "./AuthenticationResponseDTO";
+
 export interface OfferResponseDTO{
     id: number;
     title: string;
     description: string;
     contrat: string;
     createdAt: Date;
+    creator:AuthenticationResponseDTO;
 }
